4d14b29ec184fb718cb1ffbfdd955fca41f6853c
Lukas Piatkowski <luk@fb.com>
2019-11-29 07:18:05 -0800

    rust-shed: move common/rust/thrift/compiler to the shed
    Reviewed By: ikostia
    
    Differential Revision: D18743244
    
    fbshipit-source-id: 3573400400439c532677835922f6537b8f2218a0

---
+++ thrift_compiler/src/lib.rs
@@ -0,0 +1,181 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License found in the LICENSE file in the root
+ * directory of this source tree.
+ */
+
+#![deny(warnings, clippy::all)]
+
+use std::env;
+use std::ffi::{OsStr, OsString};
+use std::fs::{create_dir_all, read_to_string, write};
+use std::path::{Path, PathBuf};
+use std::process::Command;
+
+use anyhow::{anyhow, ensure, Context, Result};
+
+pub struct Config {
+    thrift_bin: OsString,
+    out_dir: PathBuf,
+    base_path: Option<PathBuf>,
+    crate_map: Option<PathBuf>,
+    options: Option<String>,
+}
+
+impl Config {
+    pub fn new(thrift_bin: OsString, out_dir: PathBuf) -> Self {
+        Self {
+            thrift_bin,
+            out_dir,
+            base_path: None,
+            crate_map: None,
+            options: None,
+        }
+    }
+
+    pub fn from_env() -> Result<Self> {
+        let thrift_bin = env::var_os("THRIFT").unwrap_or_else(|| OsStr::new("thrift1").to_owned());
+        let out_dir = PathBuf::from(
+            env::var("OUT_DIR")
+                .with_context(|| anyhow!("The OUT_DIR environment variable must be set"))?,
+        );
+
+        let crate_map = out_dir.join("cratemap");
+        let mut conf = Self::new(thrift_bin, out_dir);
+
+        if crate_map.is_file() {
+            conf.crate_map(crate_map);
+        }
+
+        Ok(conf)
+    }
+
+    pub fn base_path(&mut self, value: impl Into<PathBuf>) -> &mut Self {
+        self.base_path = Some(value.into());
+        self
+    }
+
+    pub fn crate_map(&mut self, value: impl Into<PathBuf>) -> &mut Self {
+        self.crate_map = Some(value.into());
+        self
+    }
+
+    pub fn options(&mut self, value: impl Into<String>) -> &mut Self {
+        self.options = Some(value.into());
+        self
+    }
+
+    pub fn run(&self, input_files: impl IntoIterator<Item = impl AsRef<Path>>) -> Result<()> {
+        let input = name_and_path_from_input(input_files)?;
+        create_dir_all(&self.out_dir)?;
+
+        if input.len() == 1 {
+            self.run_compiler(&self.out_dir, input.into_iter().next().unwrap().1)?;
+        } else {
+            let partial_dir = self.out_dir.join("partial");
+            create_dir_all(&partial_dir)?;
+
+            write(
+                self.out_dir.join("lib.rs"),
+                input
+                    .into_iter()
+                    .map(|(name, file)| {
+                        let out = partial_dir.join(&name);
+                        create_dir_all(&out)?;
+                        Ok(format!(
+                            "pub mod {} {{\n{}\n}}\n",
+                            name.to_string_lossy(),
+                            self.run_compiler(out, file)?,
+                        ))
+                    })
+                    .collect::<Result<Vec<_>>>()?
+                    .join("\n"),
+            )?;
+        }
+
+        Ok(())
+    }
+
+    fn run_compiler(&self, out: impl AsRef<Path>, input: impl AsRef<Path>) -> Result<String> {
+        let mut cmd = Command::new(&self.thrift_bin);
+
+        let args = {
+            let mut args = Vec::new();
+            if let Some(crate_map) = &self.crate_map {
+                args.push(format!("cratemap={}", crate_map.display()))
+            }
+            if let Some(base_path) = &self.base_path {
+                args.push(format!("include_prefix={}", base_path.display()));
+                cmd.arg("-I");
+                cmd.arg(base_path);
+            }
+            if let Some(options) = &self.options {
+                args.push(options.to_owned());
+            }
+            if args.is_empty() {
+                "".to_owned()
+            } else {
+                format!(":{}", args.join(","))
+            }
+        };
+
+        cmd.arg("--gen")
+            .arg(format!("mstch_rust{}", args))
+            .arg("--out")
+            .arg(out.as_ref())
+            .arg(input.as_ref());
+
+        let output = cmd.output().with_context(|| {
+            format!(
+                "Failed to run thrift compiler. Is '{}' executable?",
+                self.thrift_bin.to_string_lossy()
+            )
+        })?;
+        ensure!(
+            output.status.success(),
+            format!(
+                "Command '{:#?}' failed! Stdout:\n{}\nStderr:\n{}",
+                cmd,
+                String::from_utf8_lossy(&output.stdout),
+                String::from_utf8_lossy(&output.stderr),
+            )
+        );
+
+        let out_file = out.as_ref().join("lib.rs");
+        ensure!(
+            out_file.is_file(),
+            format!(
+                "Thrift has successfully run, but the resulting '{}' file is missing, command: '{:#?}'",
+                out_file.display(),
+                cmd,
+            )
+        );
+
+        read_to_string(&out_file)
+            .with_context(|| format!("Failed to read content of file '{}'", out_file.display()))
+    }
+}
+
+fn name_and_path_from_input<T: AsRef<Path>>(
+    input_files: impl IntoIterator<Item = T>,
+) -> Result<Vec<(OsString, T)>> {
+    input_files
+        .into_iter()
+        .map(|file| {
+            Ok((
+                file.as_ref()
+                    .file_stem()
+                    .ok_or_else(|| {
+                        anyhow!(
+                            "Failed to get file_stem from path {}",
+                            file.as_ref().display()
+                        )
+                    })?
+                    .to_owned(),
+                file,
+            ))
+        })
+        .collect::<Result<Vec<(OsString, _)>>>()
+}
