17ec44387902e6a418b15f65d577a7dad41a2c4a
Lukas Piatkowski <luk@fb.com>
2019-12-12 10:33:29 -0800

    rust-shed: fix incorrect license header in futures_stats/test/main.rs
    Summary: (Note: this ignores all push blocking failures!)
    
    Reviewed By: ikostia
    
    Differential Revision: D18964009
    
    fbshipit-source-id: 71cd4aaf8da4321376c104afbc4034007dab4a42

--- shed/futures_stats/test/main.rs
+++ shed/futures_stats/test/main.rs
@@ -1,4 +1,10 @@
-// (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License found in the LICENSE file in the root
+ * directory of this source tree.
+ */
 
 use futures::{future, stream, Stream};
 
