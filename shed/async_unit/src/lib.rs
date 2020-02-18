a67056f69a13d16a44928d92f4e9208a0c7dd8c8
Thomas Orozco <torozco@fb.com>
2020-02-18 01:52:44 -0800

    mononoke (+ rust/shed/async_unit): update async_unit to expect async fn's
    Summary:
    This allows code that is being exercised under async_unit to call into code
    that expects a Tokio 0.2 environment (e.g. 0.2 timers).
    
    Unfortunately, this requires turning off LSAN for the async_unit tests, since
    it looks like LSAN and Tokio 0.2 don't work very well together, resulting in
    LSAN reporting leaked memory for some TLS structures that were initialized by
    tokio-preview (regardless of whether the Runtime is being dropped):
    https://fb.workplace.com/groups/rust.language/permalink/3249964938385432/
    
    Considering async_unit is effectively only used in Mononoke, and Mononoke
    already turns off LSAN in tests for precisely this reason ... it's probably
    reasonable to do the same here.
    
    The main body of changes here is also about updating the majority of our
    changes to stop calling wait(), and use this new async unit everywhere. This is
    effectively a pretty big batch conversion of all of our tests to use async fns
    instead of the former approaches. I've also updated a substantial number of
    utility functions to be async fns.
    
    A few notable changes here:
    
    - Some pushrebase tests were pretty flaky — the race they look for isn't
      deterministic. I added some actual waiting (using pushrebase hooks) to make
      it more deterministic.  This is kinda copy pasted from the globalrev hook
      (where I had introduced this first), but this will do for now.
    - The multiplexblob tests don't work at all with new futures, because they call
      `poll()` all over the place. I've updated them to new futures, which required
      a bit of reworking.
    - I took out a couple tests in async unit that were broken anyway.
    
    Reviewed By: StanislavGlebik
    
    Differential Revision: D19902539
    
    fbshipit-source-id: 352b4a531ef5fa855114c1dd8bb4d70ed967dd55

--- shed/async_unit/src/lib.rs
+++ shed/async_unit/src/lib.rs
@@ -10,103 +10,61 @@
 
 //! Provides tokio_unit_test helper function for async unit tests that use Tokio
 
-use std::panic::{catch_unwind, UnwindSafe};
-use std::sync::mpsc;
-
-use tokio::prelude::*;
+use futures_preview::future::Future;
 
 /// Helper function for async unit tests - run the given code within a Tokio context, including
 /// catching and reporting panics.
-pub fn tokio_unit_test<F, V>(func: F) -> V
+pub fn tokio_unit_test<F, V>(fut: F) -> V
 where
-    F: FnOnce() -> V + UnwindSafe + Send + 'static,
-    V: Send + 'static,
+    F: Future<Output = V>,
 {
-    let (tx, rx) = mpsc::channel();
-
-    tokio::run(future::lazy(move || {
-        let ret = catch_unwind(func);
-        tx.send(ret).expect("channel send failed");
-        Ok(())
-    }));
-
-    match rx.recv().expect("channel recv failed") {
-        Ok(v) => v,
-        Err(err) => {
-            if let Some(err_msg) = err.downcast_ref::<&str>() {
-                panic!("Test failed: {}", err_msg);
-            }
-            if let Some(err_msg) = err.downcast_ref::<String>() {
-                panic!("Test failed: {}", err_msg);
-            }
-            panic!("Test failed, cannot get full error message!");
-        }
-    }
+    let mut rt = tokio_compat::runtime::Runtime::new().unwrap();
+    rt.block_on_std(fut)
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-    use std::time::Duration;
+    use futures::future::{self};
 
     #[test]
     fn nil_async() {
-        tokio_unit_test(|| {})
+        tokio_unit_test(async {})
     }
 
     #[test]
     fn value_async() {
-        let v = tokio_unit_test(|| 123);
+        let v = tokio_unit_test(async { 123 });
         assert_eq!(v, 123);
     }
 
     #[test]
     #[should_panic(expected = "Something went wrong")]
     fn panic_value_async() {
-        let v = tokio_unit_test(|| -> i32 { panic!("Something went wrong") });
+        async fn test() -> i32 {
+            panic!("Something went wrong")
+        }
+        let v = tokio_unit_test(async { test().await });
         assert_eq!(v, 123);
     }
 
     #[test]
-    #[should_panic(expected = "Something went wrong")]
+    #[should_panic(expected = "Ow! Something went wrong!")]
     fn panic_async() {
-        tokio_unit_test(|| panic!("Ow! Something went wrong!"))
+        tokio_unit_test(async { panic!("Ow! Something went wrong!") })
     }
 
     #[test]
     fn spawn_async() {
-        tokio_unit_test(|| {
+        tokio_unit_test(async {
             let _ = tokio::spawn(future::lazy(|| Ok(())));
         })
     }
 
     #[test]
-    #[ignore] // can't handle panic/unwind in a spawned future
-    #[should_panic(expected = "Something went wrong")]
-    fn panic_spawn_async() {
-        tokio_unit_test(|| {
-            let _ = tokio::spawn(future::lazy(|| -> Result<(), ()> {
-                panic!("Something went wrong");
-            }));
-        })
-    }
-
-    #[test]
-    #[ignore] // Test failed: nested block_on: EnterError { reason: "attempted to run an executor while another executor is already running" }
-    fn timer_async() {
-        tokio_unit_test(move || {
-            tokio::runtime::Runtime::new()
-                .unwrap()
-                .block_on(
-                    tokio_timer::sleep(Duration::from_millis(100))
-                        .map_err(|err| {
-                            panic!("Timer error: {:?}", err);
-                        })
-                        .map(|_| {
-                            println!("woke up");
-                        }),
-                )
-                .expect("timer failed")
+    fn spawn_async_new_tokio() {
+        tokio_unit_test(async {
+            let _ = tokio_preview::spawn(async move { () });
         })
     }
 }
