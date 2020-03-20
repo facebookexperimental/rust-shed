8eca80678db1b39b8a06dca5d27926959c055f65
Lukas Piatkowski <luk@fb.com>
2020-03-20 08:52:30 -0700

    rust-shed: add hostname crate to the shed
    Reviewed By: StanislavGlebik
    
    Differential Revision: D20468997
    
    fbshipit-source-id: 25240c38812f848c9b152e648a491fdfc645ee56

---
+++ shed/hostname/src/lib.rs
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License found in the LICENSE file in the root
+ * directory of this source tree.
+ */
+
+#![deny(warnings, missing_docs, clippy::all, intra_doc_link_resolution_failure)]
+
+//! Crate that wraps the OSS hostname and FB internal libraries to provide
+//! hostname resolution
+
+use anyhow::Result;
+
+/// Returns hostname as reported by the system
+pub fn get_hostname() -> Result<String> {
+    #[cfg(not(fbcode_build))]
+    {
+        Ok(::hostname::get()?.to_string_lossy().into_owned())
+    }
+
+    #[cfg(fbcode_build)]
+    {
+        fbwhoami::FbWhoAmI::get()?
+            .name
+            .clone()
+            .ok_or(::anyhow::Error::msg("No hostname in fbwhoami"))
+    }
+}
