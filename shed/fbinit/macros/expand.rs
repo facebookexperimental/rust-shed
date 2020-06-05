b732b3be7afe424ad97ad3ef5f0081375691cf4d
Thomas Orozco <torozco@fb.com>
2020-06-05 11:15:58 -0700

    fbinit: tear down all the Rust before tearing down fb init
    Summary:
    I've historically observed a certain amount of weirdness around termination in
    our Rust binaries - things like races in cleaning up C++ things when the binary
    exits with an error message for example.
    
    Today by accident, I managed to get a pretty good repro: I instantiated a MySQL
    client (which instantiates a FbAsyncMysqlClient) under the hood, and observed
    that if I use it in a fbinit::test and my test fails, I deterministically get a
    5 second hang, after which Folly reports:
    
    ```
    Singleton of type facebook::common::mysql_client::FbAsyncMysqlClient has a living reference at destroyInstances time; beware! Raw pointer is 0x61a00000fc80. It is very likely that some other singleton is holding a shared_ptr to it. This singleton will be leaked (even if a shared_ptr to it is eventually released).Make sure dependencies between these singletons are properly defined.
    ```
    
    The funny thing is immediately after this message is shown and FB stuff is torn
    down, my FbAsyncMysqlClient actually gets freed (the 5 second hang is because
    Folly waits for 5 seconds before giving up on freeing the singleton).
    
    The reason for this turns out to be how we set things up in fbinit. We
    basically have a drop guard that uninitilizes FB when dropped, and the code for
    main or a test included right after its instantiation. If the function needs to
    be async, then it just gets passed to tokio::main or tokio::test to handle that.
    
    The problem with that is that it means that the dropguard is dropped before the
    runtime is. The problem with _that_ is that it means if you `spawn` anything
    that depends on FB not being uninitialized, then that stuff gets dropped after
    FB gets de-initialized, which yields to errors like the one mentioned earlier.
    
    In my MySQL case, the stuff that spawned is a BB8 connection pool, but I think
    in general we probably cannot expect Rust binaries not to spawn things that
    depend on FB being uninitialized.
    
    With all behind said, I'd like to update fbinit to drop FB after dropping the
    runtime, which is what this diff does. I've reused the same defaults as Tokio
    (basic scheduler for tests, threaded scheduler for main):
    https://github.com/tokio-rs/tokio/blob/master/tokio-macros/src/entry.rs
    
    One thing I should mention is that associating syntax errors with their source
    still works (FWIW, it didn't work for me on master, so this might be better not
    just equivalent), though it yields some slightly questionable messages:
    
    ```
    stderr: error: expected one of `!`, `.`, `::`, `;`, `?`, `{`, `}`, or an operator, found `zz`
      --> scm/metagit/metagit-server/src/main.rs:70:14
       |
    70 |     do_main  zz(fb).await
       |              ^^ expected one of 8 possible tokens
    
    error[E0308]: mismatched types
      --> scm/metagit/metagit-server/src/main.rs:68:1
       |
    68 |   #[fbinit::main]
       |   ^^^^^^^^^^^^^^^
       |   |
       |   expected enum `std::result::Result`, found `()`
       |   help: try using a variant of the expected enum: `Ok(#[fbinit::main])`
       |   in this macro invocation
       |
      ::: common/rust/shed/fbinit/macros/lib.rs:35:1
       |
    35 | / pub fn main(args: TokenStream, input: TokenStream) -> TokenStream {
    36 | |     parse_macro_input!(args as Nothing);
    37 | |     expand(Mode::Main, parse_macro_input!(input))
    38 | |         .unwrap_or_else(|err| err.to_compile_error())
    39 | |         .into()
    40 | | }
       | |_- in this expansion of `#[fbinit::main]`
       |
       = note:   expected enum `std::result::Result<(), anyhow::Error>`
               found unit type `()`
    
    error: aborting due to 2 previous errors
    
    For more information about this error, try `rustc --explain E0308`.
    ```
    
    I haven't been able to fix the "mismatched types" that's being reported here.
    The types being mismatched is fine (and correct), but reporting fbinit::main as
    the type is weird and I don't know why that's happening.
    
    Reviewed By: farnz, dtolnay
    
    Differential Revision: D21887999
    
    fbshipit-source-id: 25c7a40b42a3b80ad745dd6f4b0682b2016efd12

--- shed/fbinit/macros/expand.rs
+++ shed/fbinit/macros/expand.rs
@@ -8,10 +8,9 @@
  */
 
 use proc_macro2::TokenStream;
-use quote::{quote, quote_spanned};
+use quote::quote;
 use syn::punctuated::Punctuated;
-use syn::spanned::Spanned;
-use syn::{parse_quote, Error, Ident, ItemFn, Result};
+use syn::{parse_quote, Block, Error, ItemFn, Result};
 
 #[derive(Copy, Clone, PartialEq)]
 pub enum Mode {
@@ -48,49 +47,46 @@ pub fn expand(mode: Mode, mut function: ItemFn) -> Result<TokenStream> {
     function.sig.inputs = Punctuated::new();
 
     let block = function.block;
-    function.block = parse_quote!({
-        #guard
-        #assignment unsafe {
-            fbinit::r#impl::perform_init()
-        };
-        let _destroy_guard = unsafe { fbinit::r#impl::DestroyGuard::new() };
-        #block
-    });
 
-    if mode == Mode::CompatTest {
-        let block = function.block;
-        function.block = parse_quote!({
+    let body: Block = match (function.sig.asyncness.is_some(), mode) {
+        (true, Mode::CompatTest) => parse_quote!({
             tokio_compat::runtime::current_thread::Runtime::new().unwrap().block_on_std(async {
                 #block
             })
-        });
-        if function.sig.asyncness.is_none() {
+        }),
+        (true, Mode::Test) => parse_quote!({
+            tokio::runtime::Builder::new().basic_scheduler().enable_all().build().unwrap().block_on(async {
+                #block
+            })
+        }),
+        (true, Mode::Main) => parse_quote!({
+            tokio::runtime::Runtime::new().unwrap().block_on(async {
+                #block
+            })
+        }),
+        (false, Mode::CompatTest) => {
             return Err(Error::new_spanned(
                 function.sig,
                 "#[fbinit::compat_test] should be used only on async functions",
             ));
         }
-        function.sig.asyncness = None;
+        (false, _) => parse_quote!({ #block }),
+    };
+
+    function.block = parse_quote!({
+        #guard
+        #assignment unsafe {
+            fbinit::r#impl::perform_init()
+        };
+        let destroy_guard = unsafe { fbinit::r#impl::DestroyGuard::new() };
+        #body
+    });
+
+    function.sig.asyncness = None;
+
+    if mode == Mode::Test || mode == Mode::CompatTest {
         function.attrs.push(parse_quote!(#[test]));
-
-        Ok(quote!(#function))
-    } else {
-        if function.sig.asyncness.is_some() {
-            let tokio_attribute = match mode {
-                Mode::Main => "main",
-                Mode::Test => "test",
-                Mode::CompatTest => unreachable!(),
-            };
-            let span = function.sig.span();
-            let ident = Ident::new(tokio_attribute, span);
-            let attr = quote_spanned! {span=>
-                #[fbinit::r#impl::tokio::#ident]
-            };
-            function.attrs.push(parse_quote!(#attr));
-        } else if mode == Mode::Test {
-            function.attrs.push(parse_quote!(#[test]));
-        }
-
-        Ok(quote!(#function))
     }
+
+    Ok(quote!(#function))
 }
