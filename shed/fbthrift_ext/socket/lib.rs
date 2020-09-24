83cd969dcd87d13b72ada8c54c83c51fccf4b938
Mohan Zhang <mohanz@fb.com>
2020-09-24 10:15:18 -0700

    Remove SocketTransportLegacy and dependencies
    Reviewed By: wqfish
    
    Differential Revision: D23899877
    
    fbshipit-source-id: 9beb22d2f6248297193a1ef2d72ac9c6fa105d0f

--- shed/fbthrift_ext/socket/lib.rs
+++ shed/fbthrift_ext/socket/lib.rs
@@ -14,62 +14,19 @@ use bytes::{Bytes, BytesMut};
 use fbthrift::{Framing, FramingDecoded, FramingEncodedFinal, Transport};
 use fbthrift_framed::FramedTransport;
 use fbthrift_util::poll_with_lock;
-use futures::compat::Future01CompatExt;
-use futures::future::{FutureExt, TryFutureExt};
+use futures::future::FutureExt;
 use std::future::Future;
 use std::io::Cursor;
 use std::pin::Pin;
 use std::sync::Arc;
 use tokio::net::UnixStream;
 use tokio::sync::Mutex;
-use tokio_core::reactor::Handle;
-use tokio_proto::pipeline::ClientService;
-use tokio_proto::BindClient;
-use tokio_service::Service as _;
 use tokio_tower::pipeline::client::Client;
 use tokio_util::codec::{Decoder, Framed};
 use tower_service::Service;
 
 /// ```ignore
-/// let stream = tokio_uds::UnixStream::connect(path, handle)?;
-/// let transport = SocketTransportLegacy::new(handle, stream);
-/// let client = fb303::client::FacebookService::new(CompactProtocol, transport);
-/// ```
-pub struct SocketTransportLegacy {
-    service: ClientService<tokio_uds::UnixStream, FramedTransport>,
-}
-
-impl SocketTransportLegacy {
-    pub fn new(handle: &Handle, stream: tokio_uds::UnixStream) -> Self {
-        SocketTransportLegacy {
-            service: FramedTransport.bind_client(&handle, stream),
-        }
-    }
-}
-
-impl Framing for SocketTransportLegacy {
-    type EncBuf = BytesMut;
-    type DecBuf = Cursor<bytes_old::Bytes>;
-    type Meta = ();
-
-    fn enc_with_capacity(cap: usize) -> Self::EncBuf {
-        BytesMut::with_capacity(cap)
-    }
-
-    fn get_meta(&self) {}
-}
-
-impl Transport for SocketTransportLegacy {
-    fn call(
-        &self,
-        req: FramingEncodedFinal<Self>,
-    ) -> Pin<Box<dyn Future<Output = Result<FramingDecoded<Self>>> + Send + 'static>> {
-        self.service.call(req).compat().map_err(Error::new).boxed()
-    }
-}
-
-/// ```ignore
-/// let stream = tokio_uds::UnixStream::connect(path)?;
+/// let stream = tokio::net::UnixStream::connect(path)?;
 /// let transport = SocketTransport::new(stream);
 /// let client = fb303::client::FacebookService::new(CompactProtocol, transport);
 /// ```
