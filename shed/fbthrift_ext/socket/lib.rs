dc69cf8b1fcfed95ee7675cee88a678a3d279c2d
Zeyi (Rice) Fan <zeyi@fb.com>
2021-04-27 21:29:09 -0700

    fbthrift/socket: add a compat layer for Windows
    Summary:
    This is the magic that allows us to use UNIX domain socket on Windows.
    
    This essentially bridges async-io with tokio since async-io is able to poll a UNIX domain socket handle while mio cannot (they are still working on this).
    
    imeanyeahitworks
    
    Reviewed By: xavierd
    
    Differential Revision: D25759243
    
    fbshipit-source-id: 315cf65b9e58e604e35a97cb1be39282aa91d1db

--- shed/fbthrift_ext/socket/lib.rs
+++ shed/fbthrift_ext/socket/lib.rs
@@ -7,8 +7,6 @@
  * of this source tree.
  */
 
-#![cfg(not(target_os = "windows"))]
-
 use anyhow::{Error, Result};
 use bytes::{Bytes, BytesMut};
 use fbthrift::{Framing, FramingDecoded, FramingEncodedFinal, Transport};
@@ -19,9 +17,9 @@ use std::future::Future;
 use std::io::Cursor;
 use std::pin::Pin;
 use std::sync::Arc;
-use tokio::net::UnixStream;
 use tokio::sync::Mutex;
 use tokio_tower::pipeline::client::Client;
+use tokio_uds_compat::UnixStream;
 use tokio_util::codec::{Decoder, Framed};
 use tower_service::Service;
 
