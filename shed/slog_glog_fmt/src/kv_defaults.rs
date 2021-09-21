ee1a84168cd0f28af66afd0b8cb353b0ae05858f
Gus Wynn <azw@fb.com>
2021-09-21 12:51:09 -0700

    add to kv_defaults in glog formatter
    Differential Revision: D31064610
    
    fbshipit-source-id: d07805b4d2f5a5f8b881fa7e37fde6a0e4613618

--- shed/slog_glog_fmt/src/kv_defaults.rs
+++ shed/slog_glog_fmt/src/kv_defaults.rs
@@ -15,6 +15,7 @@ use slog::{Record, Result as SlogResult, Serializer, KV};
 /// Structure containing all common KV values for Facebook services
 pub struct FacebookKV {
     hostname: String,
+    key: &'static str,
 }
 
 impl FacebookKV {
@@ -22,13 +23,22 @@ impl FacebookKV {
     pub fn new() -> Result<Self> {
         Ok(FacebookKV {
             hostname: hostname::get()?.to_string_lossy().into_owned(),
+            key: "hostname",
+        })
+    }
+
+    /// Creates a new instance of this structure, with an overridden key string
+    pub fn with_key(key: &'static str) -> Result<Self> {
+        Ok(FacebookKV {
+            hostname: hostname::get()?.to_string_lossy().into_owned(),
+            key,
         })
     }
 }
 
 impl KV for FacebookKV {
     fn serialize(&self, _record: &Record<'_>, serializer: &mut dyn Serializer) -> SlogResult {
-        serializer.emit_str("hostname", &self.hostname)
+        serializer.emit_str(self.key, &self.hostname)
     }
 }
 
@@ -55,6 +65,23 @@ mod tests {
         let result = serializer.into_inner();
         assert!(result.len() == 1);
         assert_eq!(result[0].0, "hostname");
+        assert!(result[0].1 != "");
+    }
+
+    #[test]
+    fn test_with_key() {
+        let categorizer = InlineCategorizer;
+        let mut serializer = CollectorSerializer::new(&categorizer);
+        FacebookKV::with_key("funny")
+            .expect("failed to create kv")
+            .serialize(
+                &record!(Level::Info, "test", &format_args!(""), b!()),
+                &mut serializer,
+            )
+            .expect("failed to serialize");
+        let result = serializer.into_inner();
+        assert!(result.len() == 1);
+        assert_eq!(result[0].0, "funny");
         assert!(result[0].1 != "");
     }
 }
