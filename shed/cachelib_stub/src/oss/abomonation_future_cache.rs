277e3c9a4d6c8bb2542fec52b4215efc719b7fa3
Simon Farnsworth <simonfar@fb.com>
2022-03-08 03:10:41 -0800

    Support TTLs in cachelib variant of `caching_ext`
    Summary: I'm going to use TTLs in Mutable Renames caching. Make it work
    
    Reviewed By: mitrandir77
    
    Differential Revision: D34527803
    
    fbshipit-source-id: 3b53ee4e89153814981dcb68b838ee86f5ac7388

--- shed/cachelib_stub/src/oss/abomonation_future_cache.rs
+++ shed/cachelib_stub/src/oss/abomonation_future_cache.rs
@@ -9,6 +9,7 @@
 
 use anyhow::{Error, Result};
 use futures_ext::BoxFuture;
+use std::time::Duration;
 
 use super::lrucache::VolatileLruCachePool;
 
@@ -37,6 +38,7 @@ pub fn set_cached<T>(
     _cache_pool: &VolatileLruCachePool,
     _cache_key: &String,
     _entry: &T,
+    _ttl: Option<Duration>,
 ) -> Result<bool>
 where
     T: abomonation::Abomonation + Clone + Send + 'static,
