596edf4d2be235c4bcd806f3a9891ce19c0c5ef1
Emerson Ford <emersonford@meta.com>
2023-03-07 10:01:20 -0800

    add adapter trait
    Summary: defines the trait that must be implemented for a Rust Thrift adapter, and adds an example implementation.
    
    Reviewed By: anps77
    
    Differential Revision: D43669078
    
    fbshipit-source-id: ba6e143f4a4f4c5f986a9feb60c15fa5ce36d62d

---
+++ shed/fbthrift_ext/adapters/lib.rs
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under both the MIT license found in the
+ * LICENSE-MIT file in the root directory of this source tree and the Apache
+ * License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+ * of this source tree.
+ */
+
+use std::marker::PhantomData;
+
+use fbthrift::adapter::ThriftTypeAdapter;
+use uuid::Uuid;
+
+pub struct UuidAdapter<T> {
+    inner: PhantomData<T>,
+}
+
+impl ThriftTypeAdapter for UuidAdapter<Vec<u8>> {
+    type ThriftType = Vec<u8>;
+    type AdaptedType = Uuid;
+
+    type Error = uuid::Error;
+
+    fn to_thrift(value: &Self::AdaptedType) -> Self::ThriftType {
+        value.as_bytes().to_vec()
+    }
+
+    fn from_thrift(value: Self::ThriftType) -> Result<Self::AdaptedType, Self::Error> {
+        if value.is_empty() {
+            Ok(Uuid::nil())
+        } else {
+            Uuid::from_slice(&value)
+        }
+    }
+}
