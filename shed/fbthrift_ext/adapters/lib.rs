50d7997b917e473c6d58a914faa277662a8d3d2b
Eddie Shen <xes@meta.com>
2023-03-14 15:10:19 -0700

    Document fbthrift_ext/adapters crate root
    Summary: Adds docs.
    
    Reviewed By: anps77
    
    Differential Revision: D44065760
    
    fbshipit-source-id: 5f161f1050db29c9dd0f21ac3ee0eade888ac599

--- shed/fbthrift_ext/adapters/lib.rs
+++ shed/fbthrift_ext/adapters/lib.rs
@@ -7,6 +7,83 @@
  * of this source tree.
  */
 
+//! Thrift adapters that convert between thrift types and Rust types.
+//!
+//! Thrift adapters allow Rust code interacting with thrift types to be
+//! presented as another type instead. This allows for Rust to interact with
+//! thrift at a much higher semantic level with stronger type safety.
+//!
+//! As a result, when using an adapter, all Rust clients and services will have
+//! additional parsing performed automatically when receiving a message. All
+//! Rust clients and services will also be required to provide an adapted type
+//! instead of the original thrift type when sending a message, preventing type
+//! confusion and ensuring that what is sent is semantically correct.
+//!
+//! # Examples
+//!
+//! Using thrift adapters involve making changes to the `thrift_library` target
+//! and the thrift file itself.
+//!
+//! The following `thrift_library` target is a complete example. Please
+//! especially note the comments.
+//!
+//! ```skylark
+//! thrift_library(
+//!     name = "api",
+//!     languages = ["rust"],
+//!     rust_crate_name = "api",
+//!     rust_deps = [
+//!         // The target in which exports your adapter.
+//!         "//common/rust/shed/fbthrift_ext:adapters",
+//!     ],
+//!     // Required if using adapters, see 'Safety' section.
+//!     rust_unittests = True,
+//!     thrift_srcs = {
+//!         "api.thrift": [],
+//!     },
+//!     deps = [
+//!         // Needed to enable the @rust.Adapter annotation.
+//!         "//thrift/annotation:rust",
+//!     ],
+//! )
+//! ```
+//!
+//! Then, in your thrift file, you can begin to use your adapter:
+//!
+//! ```thrift
+//! include "thrift/annotation/rust.thrift"
+//!
+//! @rust.Adapter{name = "::fbthrift_adapters::UuidAdapter"}
+//! typedef binary uuid
+//!
+//! struct CreateWorkflowRequest {
+//!   1: uuid id;
+//! }
+//! ```
+//!
+//! The field in Rust will now be the adapted type rather than the thrift type.
+//!
+//! ```
+//! # // TODO: You could probably make this an actual thrift target
+//! # mod api {
+//! # #[derive(Default)]
+//! # pub struct CreateWorkflowRequest { pub id: uuid::Uuid }
+//! # }
+//! #
+//! use api::CreateWorkflowRequest;
+//! use uuid::Uuid;
+//!
+//! let my_req = CreateWorkflowRequest {
+//!     id: Uuid::new_v4(),
+//!     ..Default::default()
+//! };
+//! ```
+//!
+//! Please note that this is not the only workflow possible. See the Adapters
+//! wiki under the Rust @ Meta internal wiki for more information.
+//!
+//! All adapters are re-exported at the root level for easy usage in thrift.
+
 pub mod duration;
 pub mod uuid;
 
