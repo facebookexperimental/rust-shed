154368395ab9ce58b71668ab06ac799be099c027
Eddie Shen <xes@meta.com>
2023-03-14 15:10:19 -0700

    Add UUID adapter tests
    Summary: Tests are good.
    
    Reviewed By: anps77
    
    Differential Revision: D44044005
    
    fbshipit-source-id: 984d20664391d88fe0fc6f2536b93675cf99b23a

--- shed/fbthrift_ext/adapters/uuid.rs
+++ shed/fbthrift_ext/adapters/uuid.rs
@@ -34,3 +34,46 @@ impl ThriftAdapter for UuidAdapter<Vec<u8>> {
         }
     }
 }
+
+#[cfg(test)]
+mod vec_impl {
+    use super::*;
+
+    type UuidAdapter = super::UuidAdapter<Vec<u8>>;
+
+    #[test]
+    fn round_trip_non_empty() {
+        #[rustfmt::skip]
+        let bytes = vec![
+            0xa1, 0xa2, 0xa3, 0xa4,
+            0xb1, 0xb2,
+            0xc1, 0xc2,
+            0xd1, 0xd2, 0xd3, 0xd4, 0xd5, 0xd6, 0xd7, 0xd8,
+        ];
+        let adapted = UuidAdapter::from_thrift(bytes.clone()).unwrap();
+        assert_eq!(UuidAdapter::to_thrift(&adapted), bytes);
+    }
+
+    #[test]
+    fn empty() {
+        let adapted = UuidAdapter::from_thrift(vec![]).unwrap();
+        assert_eq!(adapted, Uuid::nil());
+        assert_eq!(UuidAdapter::to_thrift(&adapted), vec![0; 16]);
+    }
+
+    #[test]
+    fn invalid_uuid() {
+        // uuids need to be 16 bytes long
+        assert!(UuidAdapter::from_thrift(b"hello world".to_vec()).is_err());
+        #[rustfmt::skip]
+        let bytes = vec![
+            0xa1, 0xa2, 0xa3, 0xa4, b'-',
+            0xb1, 0xb2, b'-',
+            0xc1, 0xc2, b'-',
+            0xd1, 0xd2, 0xd3, 0xd4, 0xd5, 0xd6, 0xd7, 0xd8,
+        ];
+        assert!(UuidAdapter::from_thrift(bytes).is_err());
+        // uuids need to have valid bytes
+        assert!(UuidAdapter::from_thrift(vec![0xff, 16]).is_err());
+    }
+}
