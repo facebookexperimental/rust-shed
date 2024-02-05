68e075275da4b0ee18e86d4a89d98c10a61deab5
Mark Juggurnauth-Thomas <mbthomas@meta.com>
2024-02-05 05:27:47 -0800

    cachelib_stub: remove get_cached_or_fill
    Summary: This function is unused and depends on old futures.  Remove it.
    
    Reviewed By: dtolnay
    
    Differential Revision: D53352013
    
    fbshipit-source-id: 13f62662663a8a561cc5c6842bed95eadd3ff2db

--- shed/cachelib_stub/src/oss/abomonation_future_cache.rs
+++ shed/cachelib_stub/src/oss/abomonation_future_cache.rs
@@ -9,27 +9,14 @@
 
 use std::time::Duration;
 
-use anyhow::Error;
+use abomonation::Abomonation;
 use anyhow::Result;
-use futures_ext::BoxFuture;
 
 use super::lrucache::VolatileLruCachePool;
 
-pub fn get_cached_or_fill<T, F>(
-    _cache_pool: &VolatileLruCachePool,
-    _cache_key: String,
-    fetch: F,
-) -> BoxFuture<Option<T>, Error>
-where
-    T: abomonation::Abomonation + Clone + Send + 'static,
-    F: FnOnce() -> BoxFuture<Option<T>, Error>,
-{
-    fetch()
-}
-
 pub fn get_cached<T>(_cache_pool: &VolatileLruCachePool, _cache_key: &String) -> Result<Option<T>>
 where
-    T: abomonation::Abomonation + Clone + Send + 'static,
+    T: Abomonation + Clone + Send + 'static,
 {
     Ok(None)
 }
@@ -43,7 +30,7 @@ pub fn set_cached<T>(
     _ttl: Option<Duration>,
 ) -> Result<bool>
 where
-    T: abomonation::Abomonation + Clone + Send + 'static,
+    T: Abomonation + Clone + Send + 'static,
 {
     Ok(false)
 }
