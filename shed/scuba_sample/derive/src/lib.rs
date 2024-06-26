bf0cd6c65e63dddf7d373ffe1530d28875755f43
David Tolnay <dtolnay@meta.com>
2024-06-26 10:20:14 -0700

    Update scuba_sample_derive to syn 2
    Summary: Release notes: https://github.com/dtolnay/syn/releases/tag/2.0.0
    
    Reviewed By: diliop
    
    Differential Revision: D59038431
    
    fbshipit-source-id: bb4ed51f1b34a0ed2751baa9fe4206755859c4cb

--- shed/scuba_sample/derive/src/lib.rs
+++ shed/scuba_sample/derive/src/lib.rs
@@ -8,31 +8,38 @@
  */
 
 use std::collections::HashSet;
+use std::fmt;
+use std::fmt::Display;
 
-use darling::FromField;
-use itertools::Either;
-use itertools::Itertools;
 use proc_macro::TokenStream;
 use proc_macro2::Span;
 use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
+use syn::parse::Error;
+use syn::parse::Result;
+use syn::parse_macro_input;
 use syn::Data;
 use syn::DataStruct;
+use syn::DeriveInput;
+use syn::ExprPath;
+use syn::Field;
 use syn::Fields;
+use syn::Ident;
 use syn::Lifetime;
+use syn::LitStr;
+use syn::Type;
+
+enum Derive {
+    StructuredSample,
+    TryFromSample,
+}
 
 #[proc_macro_derive(StructuredSample, attributes(scuba))]
 pub fn structured_sample_derive(input: TokenStream) -> TokenStream {
-    // Construct a representation of Rust code as a syntax tree
-    // that we can manipulate
-    match syn::parse(input) {
-        Ok(ast) => match impl_structured_sample(&ast) {
-            Ok(v) => v,
-            Err(error) => error.write_errors(),
-        },
-        Err(error) => syn::Error::to_compile_error(&error),
-    }
-    .into()
+    let ast = parse_macro_input!(input as DeriveInput);
+    impl_structured_sample(&ast)
+        .unwrap_or_else(syn::Error::into_compile_error)
+        .into()
 }
 
 /// Derives `TryFrom<ScubaSample>` for some struct.
@@ -104,38 +111,25 @@ pub fn structured_sample_derive(input: TokenStream) -> TokenStream {
 /// ```
 #[proc_macro_derive(TryFromSample, attributes(scuba))]
 pub fn try_from_sample_derive(input: TokenStream) -> TokenStream {
-    // Construct a representation of Rust code as a syntax tree
-    // that we can manipulate
-    match syn::parse(input) {
-        Ok(ast) => match impl_try_from_sample(&ast) {
-            Ok(v) => v,
-            Err(error) => error.write_errors(),
-        },
-        Err(error) => syn::Error::to_compile_error(&error),
-    }
-    .into()
+    let ast = parse_macro_input!(input as DeriveInput);
+    impl_try_from_sample(&ast)
+        .unwrap_or_else(syn::Error::into_compile_error)
+        .into()
 }
 
-fn impl_structured_sample(ast: &syn::DeriveInput) -> darling::Result<TokenStream2> {
-    let name = &ast.ident;
-    let mut error_collector = darling::Error::accumulator();
-
-    let (fields, field_parse_errors): (Vec<SampleField>, Vec<darling::Error>) =
-        get_fields(ast, true)?;
-
-    for err in field_parse_errors {
-        error_collector.push(err);
-    }
-
-    let field_name = get_field_names(&fields);
-    let field_renames = get_field_renames(&fields);
+fn impl_structured_sample(ast: &DeriveInput) -> Result<TokenStream2> {
+    let (fields, mut errors) = get_fields(ast, Derive::StructuredSample)?;
 
     // check for duplicate names
-    check_unique(&fields, &mut error_collector);
+    check_unique(&fields, &mut errors);
+    propagate_errors(errors)?;
+
+    let name = &ast.ident;
+    let field_name = get_field_names(&fields);
+    let field_renames = get_field_renames(&fields);
     let ty_gen = get_lifetime(ast);
 
-    error_collector.finish()?;
-    let gen = quote! {
+    Ok(quote! {
         impl ::scuba_sample::StructuredSample for #name #ty_gen {}
 
         impl ::core::convert::From<#name #ty_gen> for ::scuba_sample::ScubaSample {
@@ -147,21 +141,17 @@ fn impl_structured_sample(ast: &syn::DeriveInput) -> darling::Result<TokenStream
                 sample
             }
         }
-    };
-    Ok(gen)
+    })
 }
 
-fn impl_try_from_sample(ast: &syn::DeriveInput) -> darling::Result<TokenStream2> {
+fn impl_try_from_sample(ast: &DeriveInput) -> Result<TokenStream2> {
+    let (fields, mut errors) = get_fields(ast, Derive::TryFromSample)?;
+
+    // check for duplicate names
+    check_unique(&fields, &mut errors);
+    propagate_errors(errors)?;
+
     let name = &ast.ident;
-    let mut error_collector = darling::Error::accumulator();
-
-    let (fields, field_parse_errors): (Vec<SampleField>, Vec<darling::Error>) =
-        get_fields(ast, false)?;
-
-    for err in field_parse_errors {
-        error_collector.push(err);
-    }
-
     let field_name = get_field_names(&fields);
     let field_renames = get_field_renames(&fields);
     let field_parsers = fields
@@ -169,11 +159,7 @@ fn impl_try_from_sample(ast: &syn::DeriveInput) -> darling::Result<TokenStream2>
         .map(|field| field.get_parser())
         .collect::<Vec<_>>();
 
-    // check for duplicate names
-    check_unique(&fields, &mut error_collector);
-
-    error_collector.finish()?;
-    let gen = quote! {
+    Ok(quote! {
         impl ::scuba_sample::TryFromSample for #name {}
 
         impl ::core::convert::TryFrom<::scuba_sample::ScubaSample> for #name {
@@ -185,8 +171,8 @@ fn impl_try_from_sample(ast: &syn::DeriveInput) -> darling::Result<TokenStream2>
                         .retrieve(#field_renames)
                         .ok_or_else(|| ::scuba_sample::Error::MissingColumn(
                             ::std::format!(
-                                "Could not find {} in ScubaSample {:?}",
-                                ::core::stringify!(#field_renames),
+                                "Could not find {:?} in ScubaSample {:?}",
+                                #field_renames,
                                 sample,
                             ),
                         ))?;
@@ -198,79 +184,63 @@ fn impl_try_from_sample(ast: &syn::DeriveInput) -> darling::Result<TokenStream2>
                 })
             }
         }
-    };
-    Ok(gen)
+    })
 }
 
-fn get_fields(
-    ast: &syn::DeriveInput,
-    can_skip: bool,
-) -> darling::Result<(Vec<SampleField>, Vec<darling::Error>)> {
-    let fields = match &ast.data {
-        Data::Struct(DataStruct {
-            fields: Fields::Named(fields),
-            ..
-        }) => &fields.named,
-        // These branches are like this to make better compiler errors.
-        Data::Struct(s) => {
-            return Err(
-                darling::Error::unsupported_shape("struct without named fields")
-                    .with_span(&s.struct_token.span),
-            );
-        }
-        Data::Enum(s) => {
-            return Err(darling::Error::unsupported_shape("enum").with_span(&s.enum_token.span));
-        }
-        Data::Union(s) => {
-            return Err(darling::Error::unsupported_shape("union").with_span(&s.union_token.span));
-        }
+fn get_fields(ast: &DeriveInput, derive: Derive) -> Result<(Vec<SampleField>, Vec<Error>)> {
+    let Data::Struct(DataStruct {
+        fields: Fields::Named(ast_fields),
+        ..
+    }) = &ast.data
+    else {
+        return Err(Error::new_spanned(
+            ast,
+            format!("derive({}) requires a struct with named fields", derive),
+        ));
     };
 
-    Ok(fields
-        .into_iter()
-        .filter_map(|f| {
-            let field = SampleField::from_field(f);
-            if let Ok(pfield) = &field {
-                if can_skip && pfield.skip {
-                    return None;
-                }
-            }
-            Some(field)
-        })
-        .partition_map(|f| match f {
-            Ok(v) => Either::Left(v),
-            Err(v) => Either::Right(v),
-        }))
+    let can_skip = match derive {
+        Derive::StructuredSample => true,
+        Derive::TryFromSample => false,
+    };
+
+    let mut sample_fields = Vec::new();
+    let mut errors = Vec::new();
+    for field in &ast_fields.named {
+        match SampleField::from_field(field) {
+            Ok(sample_field) if can_skip && sample_field.skip => {}
+            Ok(sample_field) => sample_fields.push(sample_field),
+            Err(error) => errors.push(error),
+        }
+    }
+    Ok((sample_fields, errors))
 }
 
-fn get_field_names<'a>(fields: &'a [SampleField]) -> Vec<&'a Option<syn::Ident>> {
-    fields.iter().map(|field| &field.ident).collect::<Vec<_>>()
+fn get_field_names(fields: &[SampleField]) -> Vec<&Ident> {
+    fields.iter().map(|field| &field.ident).collect()
 }
 
 fn get_field_renames(fields: &[SampleField]) -> Vec<String> {
     fields
         .iter()
         .map(|field| field.scuba_column_name())
-        .collect::<Vec<_>>()
+        .collect()
 }
 
-fn check_unique(fields: &[SampleField], error_collector: &mut darling::error::Accumulator) {
+fn check_unique(fields: &[SampleField], errors: &mut Vec<Error>) {
     let mut unique = HashSet::new();
     for field in fields {
         let rename = field.scuba_column_name();
-        if !unique.insert(rename) {
-            error_collector.push(
-                darling::Error::custom(format!(
-                    "duplicate scuba column name: {}",
-                    field.scuba_column_name()
-                ))
-                .with_span(&field.ident.as_ref().unwrap().span()),
-            );
+        if !unique.insert(rename.clone()) {
+            errors.push(Error::new_spanned(
+                &field.ident,
+                format!("duplicate scuba column name: {}", rename),
+            ));
         }
     }
 }
 
-fn get_lifetime(ast: &syn::DeriveInput) -> TokenStream2 {
+fn get_lifetime(ast: &DeriveInput) -> TokenStream2 {
     let mut new_gen = ast.generics.clone();
     new_gen
         .lifetimes_mut()
@@ -279,27 +249,62 @@ fn get_lifetime(ast: &syn::DeriveInput) -> TokenStream2 {
     quote! {#ty_gen}
 }
 
-#[derive(Debug, Clone, FromField)]
-#[darling(attributes(scuba), forward_attrs(allow, doc, cfg))]
+#[derive(Debug, Clone)]
 struct SampleField {
-    ident: Option<syn::Ident>,
-    ty: syn::Type,
-    name: Option<String>,
-    #[darling(default)]
+    ident: Ident,
+    ty: Type,
+    rename: Option<LitStr>,
     skip: bool,
-    custom_parser: Option<syn::Ident>,
+    custom_parser: Option<ExprPath>,
 }
 
 impl SampleField {
+    fn from_field(field: &Field) -> Result<Self> {
+        let mut rename = None;
+        let mut skip = false;
+        let mut custom_parser = None;
+        for attr in &field.attrs {
+            // Parse #[scuba(...)]
+            if attr.path().is_ident("scuba") {
+                attr.parse_nested_meta(|meta| {
+                    if meta.path.is_ident("name") {
+                        // #[scuba(name = "...")]
+                        rename = Some(meta.value()?.parse()?);
+                        Ok(())
+                    } else if meta.path.is_ident("skip") {
+                        // #[scuba(skip)]
+                        skip = true;
+                        Ok(())
+                    } else if meta.path.is_ident("custom_parser") {
+                        // #[scuba(custom_parser = "path::to")]
+                        let lit: LitStr = meta.value()?.parse()?;
+                        custom_parser = Some(lit.parse()?);
+                        Ok(())
+                    } else {
+                        Err(meta.error("unrecognized scuba attribute"))
+                    }
+                })?;
+            }
+        }
+
+        Ok(SampleField {
+            ident: field.ident.as_ref().unwrap().clone(),
+            ty: field.ty.clone(),
+            rename,
+            skip,
+            custom_parser,
+        })
+    }
+
     fn scuba_column_name(&self) -> String {
-        self.name
-            .as_ref()
-            .unwrap_or(&self.ident.as_ref().unwrap().to_string().replace('"', ""))
-            .to_string()
+        match &self.rename {
+            Some(rename) => rename.value(),
+            None => self.ident.to_string(),
+        }
     }
 
     fn get_parser(&self) -> TokenStream2 {
-        let field_name = self.ident.as_ref().unwrap();
+        let field_name = &self.ident;
         let ty = &self.ty;
         match self.custom_parser {
             Some(ref parser) => quote! {
@@ -310,4 +315,22 @@ impl SampleField {
             },
         }
     }
+}
+
+impl Display for Derive {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str(match self {
+            Derive::StructuredSample => "StructuredSample",
+            Derive::TryFromSample => "TryFromSample",
+        })
+    }
+}
+
+fn propagate_errors(errors: Vec<Error>) -> Result<()> {
+    let mut iter = errors.into_iter();
+    let Some(mut combined) = iter.next() else {
+        return Ok(());
+    };
+    combined.extend(iter);
+    Err(combined)
 }
