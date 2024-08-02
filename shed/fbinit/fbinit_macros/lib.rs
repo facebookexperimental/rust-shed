fd261e0b0ab851fc11d7b976a7bb89d84f556791
Eric Kuecks <ekuecks@meta.com>
2024-08-02 10:43:05 -0700

    Add doc comments
    Summary: The code had normal comments instead of doc comments so there is no generated documentation
    
    Reviewed By: ahornby
    
    Differential Revision:
    D60621856
    
    Privacy Context Container: L1122763
    
    fbshipit-source-id: 01bfc2c3454ad20031e122e18ff5a7baa9d0d3df

--- shed/fbinit/fbinit_macros/lib.rs
+++ shed/fbinit/fbinit_macros/lib.rs
@@ -24,82 +24,82 @@ use crate::args::Args;
 use crate::expand::expand;
 use crate::expand::Mode;
 
-// Expand from:
-//
-//     #[fbinit::main]
-//     fn main(fb: FacebookInit) {
-//         ...
-//     }
-//
-// to:
-//
-//     fn main() {
-//         let fb: FacebookInit = fbinit::perform_init();
-//         ...
-//     }
-//
-// If async, also add a #[tokio::main] attribute.
-//
-// Accepts optional attribute argument disable_fatal_signals to disable adding
-// handler to fatal signals in perform_init().
-// Argument must be one of `default`, `none`, `all`, `sigterm_only`
-// that represents the signal bit mask. For  example, the following disables SIGTERM:
-//
-//      #[fbinit::main(disable_fatal_signals = sigterm_only)
-//
-// - `default`: disables SIGTERM and SIGINT, and is also the default if `disable_fatal_signals`
-//  is not specified
-// - `none`: disabled no signals, overrides the default
-// - `all`: disables ALL signals
-// - `sigterm_only`: disabled SIGTERM
+/// Expand from:
+///
+///     #[fbinit::main]
+///     fn main(fb: fbinit::FacebookInit) {
+///         ...
+///     }
+///
+/// to:
+///
+///     fn main() {
+///         let fb: fbinit::FacebookInit = fbinit::perform_init();
+///         ...
+///     }
+///
+/// If async, also add a #[tokio::main] attribute.
+///
+/// Accepts optional attribute argument disable_fatal_signals to disable adding
+/// handler to fatal signals in perform_init().
+/// Argument must be one of `default`, `none`, `all`, `sigterm_only`
+/// that represents the signal bit mask. For  example, the following disables SIGTERM:
+///
+///      #[fbinit::main(disable_fatal_signals = sigterm_only)
+///
+/// - `default`: disables SIGTERM and SIGINT, and is also the default if `disable_fatal_signals`
+///  is not specified
+/// - `none`: disabled no signals, overrides the default
+/// - `all`: disables ALL signals
+/// - `sigterm_only`: disabled SIGTERM
 #[proc_macro_attribute]
 pub fn main(attr: TokenStream, input: TokenStream) -> TokenStream {
     do_expand(Mode::Main, attr, input)
 }
 
-// Same thing, expand:
-//
-//     #[fbinit::test]
-//     fn name_of_test(fb: FacebookInit) {
-//         ...
-//     }
-//
-// to:
-//
-//     #[test]
-//     fn name_of_test() {
-//         let fb: FacebookInit = fbinit::perform_init();
-//         ...
-//     }
-//
-// with either #[test] or #[tokio::test] attribute.
-//
-// Accepts optional attribute argument disable_fatal_signals to disable adding
-// handler to fatal signals in perform_init().
-// Argument must be an int literal that represents the signal bit mask. For
-// example, the following disables SIGTERM:
-//
-//      #[fbinit::test(disable_fatal_signals = 0x8000)
+/// Similar to #[fbinit::main], expands from:
+///
+///     #[fbinit::test]
+///     fn name_of_test(fb: fbinit::FacebookInit) {
+///         ...
+///     }
+///
+/// to:
+///
+///     #[test]
+///     fn name_of_test() {
+///         let fb: FacebookInit = fbinit::perform_init();
+///         ...
+///     }
+///
+/// with either #[test] or #[tokio::test] attribute.
+///
+/// Accepts optional attribute argument disable_fatal_signals to disable adding
+/// handler to fatal signals in perform_init().
+/// Argument must be an int literal that represents the signal bit mask. For
+/// example, the following disables SIGTERM:
+///
+///      #[fbinit::test(disable_fatal_signals = 0x8000)
 #[proc_macro_attribute]
 pub fn test(attr: TokenStream, input: TokenStream) -> TokenStream {
     do_expand(Mode::Test, attr, input)
 }
 
-// Similar to #[fbinit::test], but allows for nesting with other test attributes (e.g. #[rstest]) that wraps #[test].
+/// Similar to #[fbinit::test], but allows for nesting with other test attributes (e.g. #[rstest]) that wraps #[test].
 //
-//     #[fbinit::nested_test]
-//     #[rstest]
-//     fn name_of_test(fb: FacebookInit, some_fixture: u32) {
-//         ...
-//     }
+///     #[fbinit::nested_test]
+///     #[rstest::rstest]
+///     fn name_of_test(fb: fbinit::FacebookInit, some_fixture: u32) {
+///         ...
+///     }
 //
-// to:
+/// to:
 //
-//     #[rstest]
-//     fn name_of_test(some_fixture: u32) {
-//         let fb: FacebookInit = fbinit::perform_init();
-//         ...
-//     }
+///     #[rstest::rstest]
+///     fn name_of_test(some_fixture: u32) {
+///         let fb: fbinit::FacebookInit = fbinit::perform_init();
+///         ...
+///     }
 #[proc_macro_attribute]
 pub fn nested_test(attr: TokenStream, input: TokenStream) -> TokenStream {
     do_expand(Mode::NestedTest, attr, input)
