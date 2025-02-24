cfe000f34c178dfd88beb690a997c284319d08fb
Youssef Ibrahim <youssefsalama@meta.com>
2025-02-24 10:43:20 -0800

    move bounded_traversal from mononoke directory to common/rust/shed
    Summary: I'd like to make use of the `bounded_traversal` crate outside of mononoke's codebase to limit memory usage.
    
    Reviewed By: mitrandir77, liubov-dmitrieva
    
    Differential Revision: D70098058
    
    fbshipit-source-id: 3f0cf6d007ef7bc3d37b4bbf4ecc28b62005708d

---
+++ shed/bounded_traversal/src/lib.rs
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under both the MIT license found in the
+ * LICENSE-MIT file in the root directory of this source tree and the Apache
+ * License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+ * of this source tree.
+ */
+
+//! Read the documentation of [bounded_traversal](crate::bounded_traversal),
+//! [bounded_traversal_dag](crate::bounded_traversal_dag) and
+//! [bounded_traversal_stream](crate::bounded_traversal_stream)
+
+#[macro_use]
+mod error;
+pub use error::BoundedTraversalError;
+
+mod tree;
+pub use tree::bounded_traversal;
+
+mod dag;
+pub use dag::bounded_traversal_dag;
+pub use dag::bounded_traversal_dag_limited;
+
+mod stream;
+pub use stream::bounded_traversal_stream;
+pub use stream::limited_by_key_shardable;
+
+mod ordered_stream;
+pub use ordered_stream::bounded_traversal_limited_ordered_stream;
+pub use ordered_stream::bounded_traversal_ordered_stream;
+
+mod common;
+pub use common::OrderedTraversal;
+
+#[cfg(test)]
+mod tests;
+
+/// A type used frequently in fold-like invocations inside this module
+pub type Iter<Out> = std::iter::Flatten<std::vec::IntoIter<Option<Out>>>;
